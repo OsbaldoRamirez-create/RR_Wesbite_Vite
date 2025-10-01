f7367bdc331c38261d85af755f7ac5f8e5639213
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-10-01 00:04:55 -0700

    slideshow images

---
+++ src/Components/SlideImages.jsx
@@ -0,0 +1,27 @@
+import image1 from '../assets/SlabsLandscape.png';
+import image2 from '../assets/RetainingWallAndGrass.png';
+import image3 from '../assets/BackLawn.png';
+import image4 from '../assets/BarkAndRockSlabs.png';
+import image5 from '../assets/fence.png';
+import image6 from "../assets/Grass.jpg";
+import image7 from "../assets/LongFence.jpg";
+import image8 from "../assets/Mulch.jpg";
+import image9 from "../assets/Patio.jpg";
+import image10 from "../assets/CleanTree.jpg";
+
+export const yardImages = [{src: image1, alt:"Front Yard Slabs", text:"Rocks Slabs Landscape"},
+{src: image2, alt:"Retaining wall", text:"Retaining Wall and Artifical Lawn"},
+{src: image3, alt:"Backyard Lawn", text:"New Back Yard Sod"},
+{src: image4, alt:"Bark and slabs", text:"Bark and Rock Slabs"},
+{src: image5, alt:"Backyard Fence", text:"New Redwood fence"}
+]
+
+
+export const yardImages2 = [
+    {src: image6, alt:"Front yard sod", text:"New Front Yard Sod"},
+    {src: image7, alt:"Long Fence", text:"70 Foot Fence"},
+    {src: image8, alt:"Front Yard mulch", text:"Front Yard Mulch and Clean-Up"},
+    {src: image9, alt:"Brick Patio", text:"Brick Patio"},
+    {src: image10, alt:"Clean Tree", text:"Perfectly Trimmed Tree"}
+    ]
+    
