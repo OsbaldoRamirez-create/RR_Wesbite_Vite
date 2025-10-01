7ea2b4b03d560ba607b3aaead5154f3bbb55e50d
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-09-30 23:14:17 -0700

    created navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from 'react-router-dom'
+import '../Styles/NavbarStyles.css';
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div className='nav-text'>
+        <NavLink className='link' to='/'>Home</NavLink>
+        <div className='dropdown'>
+
+            <NavLink className='dropbtn' to="/About">About us</NavLink>
+            <div className='dropdown-menu'>
+                <NavLink className='drop-link' to="/maintenance">Maintenance Services </NavLink>
+                <NavLink className='drop-link' to="/landscaping">Landscaping Services </NavLink>
+                
+            </div>
+        </div>
+                <NavLink className='link' to="/contact">Contact Us</NavLink>
+        </div>
+    </nav>
+  )
+}
