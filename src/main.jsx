ee097dc4c98b95b951f6cea3254cc6b75a6f89ac
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-10-01 21:07:59 -0700

    removed extra lines

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Navbar from './Components/Navbar.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Home from './Pages/Home.jsx'
@@ -10,7 +9,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Navbar />
-      <App />
       <Home />
     </BrowserRouter>
   </StrictMode>,
