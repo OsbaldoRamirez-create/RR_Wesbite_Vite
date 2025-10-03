7b1bfdaae32d3c754c615c03013a604395bb059b
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-10-02 22:20:51 -0700

    added footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import '../Styles/FooterBarStyles.css';
+import { NavLink } from 'react-router-dom';
+
+export default function FooterBar() {
+  return (
+    <footer>
+        <NavLink to="/">Home</NavLink>                
+        <NavLink to="/about">About</NavLink>
+        {/* <NavLink to="/services">Services</NavLink> */}
+        <NavLink to="/contact">Contact us</NavLink>
+        <p>© Copyright by Ramirez Landscaping</p>
+    </footer>
+  )
+}
