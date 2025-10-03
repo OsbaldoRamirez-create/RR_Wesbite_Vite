cd6cafca1d39d8ce3f751147089fd85d4e29b448
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-10-02 22:21:11 -0700

    add footer

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import Navbar from './Components/Navbar.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Home from './Pages/Home.jsx'
+import Footer from './Components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Navbar />
       <Home />
+      <Footer/>
     </BrowserRouter>
   </StrictMode>,
 )
