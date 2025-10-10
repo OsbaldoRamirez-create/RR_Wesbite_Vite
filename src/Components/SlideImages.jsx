096cca7af7d7c513b926666ee75fdad3b500e1c0
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-10-10 00:14:41 -0700

    added images for gallery page

--- src/Components/SlideImages.jsx
+++ src/Components/SlideImages.jsx
@@ -8,6 +8,14 @@ import image7 from "../assets/LongFence.jpg";
 import image8 from "../assets/Mulch.jpg";
 import image9 from "../assets/Patio.jpg";
 import image10 from "../assets/CleanTree.jpg";
+import image11 from "../assets/frontLawn.png";
+import image12 from "../assets/fountainPlants.png";
+import image13 from "../assets/frontFence.png";
+import image14 from "../assets/LAS_Back.png";
+import image15 from "../assets/SCT_Front.png";
+import image16 from "../assets/planterBox.png";
+import image17 from "../assets/MPA_Roses.png";
+import image18 from "../assets/frontMulch.png";
 
 export const yardImages = [{src: image1, alt:"Front Yard Slabs", text:"Rocks Slabs Landscape"},
 {src: image2, alt:"Retaining wall", text:"Retaining Wall and Artifical Lawn"},
@@ -24,4 +32,28 @@ export const yardImages2 = [
     {src: image9, alt:"Brick Patio", text:"Brick Patio"},
     {src: image10, alt:"Clean Tree", text:"Perfectly Trimmed Tree"}
     ]
-    
+
+export const allImages = [
+    {src: image1, alt:"Front Yard Slabs", text:"Rocks Slabs Landscape", category: "Landscaping"},
+    {src: image2, alt:"Retaining wall", text:"Retaining Wall and Artifical Lawn", category: "Landscaping"},
+    {src: image3, alt:"Backyard Lawn", text:"New Back Yard Sod", category: "Maintenance"},
+    {src: image4, alt:"Bark and slabs", text:"Bark and Rock Slabs", category: "Landscaping"},
+    {src: image5, alt:"Backyard Fence", text:"New Redwood fence", category: "Fence"},
+    {src: image6, alt:"Front yard sod", text:"New Front Yard Sod", category: "Maintenance"},
+    {src: image7, alt:"Long Fence", text:"70 Foot Fence", category: "Fence"},
+    {src: image8, alt:"Front Yard mulch", text:"Front Yard Mulch and Clean-Up", category: "Maintenance"},
+    {src: image9, alt:"Brick Patio", text:"Brick Patio", category: "Pavers"},
+    {src: image10, alt:"Clean Tree", text:"Perfectly Trimmed Tree", category: "Tree"},
+    {src: image11, alt:"Front Lawn", text:"Freshly Mowed Front Lawn", category: "Maintenance"},
+    {src: image12, alt:"Fountain Plants", text:"Beautiful Fountain with Plants", category: "Landscaping"},
+    {src: image13, alt:"Front Fence", text:"New Front Yard Fence", category: "Fence"},
+    {src: image14, alt:"Back Yard Landscape", text:"Back Yard Landscape Design", category: "Maintenance"},
+    {src: image15, alt:"Front Yard Clean Up", text:"Front Yard Clean Up and New Plants", category: "Landscaping"},
+    {src: image16, alt:"Planter Box", text:"Custom Wooden Planter Box", category: "Landscaping"},
+    {src: image17, alt:"Roses", text:"Beautiful Rose Bushes", category: "Maintenance"},
+    {src: image18, alt:"Front Mulch", text:"Fresh Mulch and Clean-Up", category: "Maintenance"},
+
+
+
+    // Add more images as needed
+    ]
